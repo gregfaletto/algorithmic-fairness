503d34a35380052600873ea8ca8ae3e410066616
Greg Faletto <gfaletto@gmail.com>
2021-11-22 12:21:26 -0800

    Update fairness.R

--- fairness.R
+++ fairness.R
@@ -1,48 +1,51 @@
+# Global parameters: number of qualified and unqualified applicants from groups
+# A and B.
 
+Q_A <- 4
+N_A <- 8
+Q_B <- 4
+N_B <- 12
 
-q_a <- 4
-n_a <- 8
-q_b <- 4
-n_b <- 12
-
-# demographic parity: fine as long as proportion of students we admit
-# from each group is equal. for example...
-
+# demographic parity: satisfied as long as proportion of applicants we admit
+# from each group is equal.
 
 checkDemParity <- function(a_qa, a_na, a_qb, a_nb){
-  return((a_qa + a_na)/(q_a + n_a) == (a_qb + a_nb)/(q_b + n_b))
+  return((a_qa + a_na)/(Q_A + N_A) == (a_qb + a_nb)/(Q_B + N_B))
 }
 
 checkDemParity(a_qa = 2, a_na = 4, a_qb = 2, a_nb = 6)
 
-# equalized odds
+# equalized odds: the proportion of qualified applicants from group A who are
+# admitted should be equal to the proportion of qualified applicants from group
+# B who are admitted (and likewise for unqualified applicants)
 
 checkEO <- function(a_qa, a_na, a_qb, a_nb){
-  qualifed <- a_qa/q_a == a_qb/q_b
-  unqualified <- a_na/n_a == a_nb/n_b
+  qualifed <- a_qa/Q_A == a_qb/Q_B
+  unqualified <- a_na/N_A == a_nb/N_B
   return(qualifed && unqualified)
 }
 
 checkEO(a_qa = 2, a_na = 4, a_qb = 2, a_nb = 6)
 
-# calibration
+# calibration: the proportion of admitted applicants from group A who are
+# qualified should be equal to the proportion of admitted applicants from group
+# B who are qualified (and likewise for rejected applicants)
 
 checkCal <- function(a_qa, a_na, a_qb, a_nb){
   admitted <- a_qa/(a_qa + a_na) == a_qb/(a_qb + a_nb)
   # Get total rejected numbers for A and B
-  r_a <- q_a + n_a - (a_qa + a_na)
-  r_b <- q_b + n_b - (a_qb + a_nb)
+  r_a <- Q_A + N_A - (a_qa + a_na)
+  r_b <- Q_B + N_B - (a_qb + a_nb)
   # Compare proportions of rejected candidates who were qualified
-  rejected <- (q_a - a_qa)/r_a == (q_b - a_qb)/r_b
+  rejected <- (Q_A - a_qa)/r_a == (Q_B - a_qb)/r_b
   return(admitted & rejected)
 }
 
 checkCal(a_qa = 2, a_na = 4, a_qb = 2, a_nb = 4)
 
 checkAssortment <- function(a_qa, a_na, a_qb, a_nb){
-  if(a_qa > q_a | a_na > n_a | a_qb > q_b | a_nb > n_b){
-    stop("a_qa > q_a | a_na > n_a | a_qb > q_b | a_nb > n_b")
-  }
+  # Check inputs
+  stopifnot(a_qa <= Q_A & a_na <= N_A & a_qb <= Q_B & a_nb <= N_B)
   ret <- c(checkDemParity(a_qa, a_na, a_qb, a_nb),
            checkEO(a_qa, a_na, a_qb, a_nb),
            checkCal(a_qa, a_na, a_qb, a_nb))
